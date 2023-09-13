6a4a419f4ec43db86ebed73718e18fa53bc16dd3
MacBook <macbook@MacBook-Pro-MacBook.local>
2023-09-13 15:15:53 +0300

    oop

--- homework_11/oop.py
+++ homework_11/oop.py
@@ -1,34 +1,46 @@
 
 class Main:
-    mydict = dict()
-    mytuple = tuple()
+    mydict = dict(name = "John", age = 36, country = "Norway")
+    mytuple = tuple([1, 2, 3])
 
-    def add_dict(self, key, item):
-        if key not in self.mydict.keys():
-            self.mydict[key] = item
-            print("done", self.mydict)
+    def add_dict(self, key, item, admin = False):
+        if admin:
+            if key not in self.mydict.keys():
+                self.mydict[key] = item
+                print("done", self.mydict)
+            else:
+                print("key already exist")
         else:
-            print("key already exist")
+            print("403")
 
-    def rem_dict(self, key):
-        self.mydict.pop(key)
-        print("Done", self.mydict)
+    def rem_dict(self, key, admin = False):
+        if admin:
+            self.mydict.pop(key)
+            print("Done", self.mydict)
+        else:
+            print("403")
 
 
-
-    def add_tuple(self, item):
-        new_list = list(self.mytuple)
-        new_list.append(item)
-        self.mytuple = tuple(new_list)
-        print("done", self.mytuple)
-
-    def count_tuple(self, item):
-        new_value = self.mytuple.count(item)
-        print("done", new_value)
-
-    def index_tuple(self, item):
-        new_index = self.mytuple.index(item)
-        print("done", new_index)
+    def add_tuple(self, item, admin = False):
+        if admin:
+            new_list = list(self.mytuple)
+            new_list.append(item)
+            self.mytuple = tuple(new_list)
+            print("done", self.mytuple)
+        else:
+            print("403")
+    def count_tuple(self, item, admin = False):
+        if admin:
+            new_value = self.mytuple.count(item)
+            print("done", new_value)
+        else:
+            print("403")
+    def index_tuple(self, item, admin = False):
+        if admin:
+            new_index = self.mytuple.index(item)
+            print("done", new_index)
+        else:
+            print("403")
 
     def getinfo(self):
         return self.mydict, self.mytuple
@@ -38,6 +50,7 @@ a = Main()
 
 LoginAdmin = "Admin"
 Password = "aaaa"
+isadmin = False
 
 def menu():
     while True:
@@ -46,26 +59,27 @@ def menu():
         if answer == "1":
             new_key = input("Type key:  ")
             new_item = input("Type value:  ")
-            a.add_dict(new_key, new_item)
+            a.add_dict(new_key, new_item, isadmin)
         elif answer == "2":
             new_value = input("Type value:  ")
-            a.add_tuple(new_value)
+            a.add_tuple(new_value, isadmin)
         elif answer == "3":
             print(a.getinfo())
         elif answer == "4":
             for_delete = input("Type key:  ")
-            a.rem_dict(for_delete)
+            a.rem_dict(for_delete, isadmin)
         elif answer == "5":
             count = input("Type value:  ")
-            a.count_tuple(count)
+            a.count_tuple(count, isadmin)
         elif answer == "6":
             index = input("Type index:  ")
-            a.index_tuple(index)
+            a.index_tuple(index, isadmin)
         elif answer == "10":
             print("exit")
             break
 
 def perm():
+    global isadmin
     while True:
         print("[1] Log in as an administrator\n[2] Log in as a user\n[0] Exit")
         user = input("= ")
@@ -73,13 +87,15 @@ def perm():
             credlogin = input("Enter login =")
             credpass = input("Enter password =")
             if credlogin == LoginAdmin and credpass == Password:
-                menu()
+                isadmin=True
+                break
             else:
                 print("Incorrect login or password")
         elif user == "2":
-            print("done", a.getinfo())1
+            print("done", a.getinfo())
 
         elif user == "0":
             print("exit")
             break
 perm()
+menu()
---
+++ homework_11/oop1.py
@@ -0,0 +1,115 @@
+class Data():
+    LoginAdmin = "Admin"
+    Password = "aaaa"
+
+    mydict = dict()
+    mytuple = tuple()
+
+    def __init__(self):
+        is_admin = self.perm()
+        if is_admin != None:
+            self.menu(admin = is_admin)
+        else:
+            pass
+
+    def add_dict(self, key, item):
+        if key not in self.mydict.keys():
+            self.mydict[key] = item
+            print("done")
+        else:
+            print("key already exist")
+
+    def rem_dict(self, key):
+        self.mydict.pop(key)
+        print("Done")
+
+    def add_tuple(self, item):
+        new_list = list(self.mytuple)
+        new_list.append(item)
+        self.mytuple = tuple(new_list)
+        print("done")
+
+    def count_tuple(self, item):
+        new_value = self.mytuple.count(item)
+        print("done")
+
+    def index_tuple(self, item):
+        self.mytuple.index(item)
+        print("done")
+
+    def show_dict(self):
+        print(self.mydict)
+
+
+    def show_tuple(self):
+        print(self.mytuple)
+
+
+    def menu(self, admin=False):
+        if admin:
+            while True:
+                print(
+                    " 1-Add dict \n 2-Add tuple \n 3-Get info \n 4-Delete item from dict \n 5-The number of repetitions of a certain value \n 6-Find the first index, for value: \n 10-Go to back panel\n 11 - Exit-EXIT")
+                answer = input("= ")
+                if answer == "1":
+                    new_key = input("Type key:  ")
+                    new_item = input("Type value:  ")
+                    self.add_dict(new_key, new_item)
+                elif answer == "2":
+                    new_value = input("Type value:  ")
+                    self.add_tuple(new_value)
+                elif answer == "3":
+                    self.show_tuple()
+                    self.show_dict()
+                elif answer == "4":
+                    for_delete = input("Type key:  ")
+                    self.rem_dict(for_delete)
+                elif answer == "5":
+                    count = input("Type value:  ")
+                    self.count_tuple(count)
+                elif answer == "6":
+                    index = input("Type index:  ")
+                    self.index_tuple(index)
+                elif answer == "10":
+                    print("exit")
+                    self.__init__()
+                elif answer == "11":
+                    print("exit")
+                    exit()
+
+        else:
+            while True:
+                answer = input('[1]  Show dict\n[2]  Show tuple\n[0] Go to back panel\n= ')
+                if answer == '1':
+                    self.show_dict()
+                elif answer == '2':
+                    self.show_tuple()
+                elif answer == '0':
+                    self.__init__()
+                else:
+                    print('401')
+
+
+    def perm(self):
+        while True:
+            print("[1] Log in as an administrator\n[2] Log in as a user\n[0] Exit")
+            user = input("= ")
+            if user == "1":
+                credlogin = input("Enter login =")
+                credpass = input("Enter password =")
+                if credlogin == self.LoginAdmin and credpass == self.Password:
+                    return True
+                else:
+                    print("Incorrect login or password")
+            elif user == "2":
+                print("done you USER")
+                return False
+
+            elif user == "0":
+                print("exit")
+                return None
+            else:
+                print("Error, plz write now")
+
+
+d = Data()

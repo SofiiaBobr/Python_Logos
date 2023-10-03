4fdc7ce174b81f1251bbe3b1fee5274e9da24bf4
Sofia B <bobrivecsofia@gmail.com>
2023-10-03 16:54:41 +0300

    refactor

--- python_django/django_4.0/env_django_4.0/bin/activate.csh
+++ python_django/django_4.0/env_django_4.0/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/macbook/Documents/Python_Logos/python_django/django_latest_version/env_latest_django"
+setenv VIRTUAL_ENV "/Users/macbook/Documents/Python_Logos/python_django/django_4.0/env_django_4.0"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
@@ -17,8 +17,8 @@ setenv PATH "$VIRTUAL_ENV/bin:$PATH"
 set _OLD_VIRTUAL_PROMPT="$prompt"
 
 if (! "$?VIRTUAL_ENV_DISABLE_PROMPT") then
-    set prompt = "(env_latest_django) $prompt"
-    setenv VIRTUAL_ENV_PROMPT "(env_latest_django) "
+    set prompt = "(env_django_4.0) $prompt"
+    setenv VIRTUAL_ENV_PROMPT "(env_django_4.0) "
 endif
 
 alias pydoc python -m pydoc
--- python_django/django_4.0/env_django_4.0/bin/activate.fish
+++ python_django/django_4.0/env_django_4.0/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/macbook/Documents/Python_Logos/python_django/django_latest_version/env_latest_django"
+set -gx VIRTUAL_ENV "/Users/macbook/Documents/Python_Logos/python_django/django_4.0/env_django_4.0"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -56,7 +56,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s%s%s" (set_color 4B8BBE) "(env_latest_django) " (set_color normal)
+        printf "%s%s%s" (set_color 4B8BBE) "(env_django_4.0) " (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
@@ -65,5 +65,5 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
     end
 
     set -gx _OLD_FISH_PROMPT_OVERRIDE "$VIRTUAL_ENV"
-    set -gx VIRTUAL_ENV_PROMPT "(env_latest_django) "
+    set -gx VIRTUAL_ENV_PROMPT "(env_django_4.0) "
 end

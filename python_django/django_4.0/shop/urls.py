1a2599f4c263afe591506778463a148787e9bf5d
Sofia B <bobrivecsofia@gmail.com>
2023-10-25 14:55:36 +0300

    frontend_and_html_imitation

--- python_django/django_4.0/shop/urls.py
+++ python_django/django_4.0/shop/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name = 'home'), # URL для домашньої сторінки, відображає в'юшку home
 
-    path('products/<str:cat>/', views.products, name = 'products')
+    path('products/<str:cat>/', views.products, name = 'products'),
+    path('products/<str:cat>/<int:id>/', views.product, name = 'product'),
+    path('home/add_product/', views.add_product, name = 'add_product')
+
 
 ]
--- python_django/django_4.0/shop/views.py
+++ python_django/django_4.0/shop/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product, Category
+from .forms import Product_Form
+from django.http import HttpResponseRedirect
+
 # Create your views here.
 def home(request):
     cats = Category.objects.all()
@@ -10,3 +13,16 @@ def products(request, cat):
     product_data = Product.objects.filter(cat__name=cat)
     return render(request, 'products.html', {"products": product_data, 'cat': cat})
 
+def product(request, cat, id):
+    product = Product.objects.get(pk=id)
+    return render(request, 'product.html', {"product": product, 'cat': cat})
+
+def add_product(request):
+    if request.method == 'POST':
+        form = Product_Form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = Product_Form()
+    return render(request, 'addproduct.html', {'form': form})
